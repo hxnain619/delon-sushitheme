d0918a3327055d0c6b9f60492178a3765a69ab92
h26k2 <hasnain.karim262@outlook.com>
2021-02-13 21:40:06 +0500

    some updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,17 +8,18 @@ class Nav extends React.Component{
                 <div className="row">
                     <div className="col s4">
                         <NavLink to="/">
-                            <i className="fas fa-search"></i>
+                            <i className="fas fa-search fa-lg"></i>
                         </NavLink>
                     </div>
                     <div className="col s4">
                         <NavLink to="/">
-                            <i className="far fa-user"></i>
+                            <i className="far fa-user fa-lg"></i>
                         </NavLink>
                     </div>
                     <div className="col s4">
                         <NavLink to="/">
-                            <i className="fas fa-shopping-cart"></i>
+                            <i className="fas fa-shopping-cart fa-lg"></i>
+                            <span className="notification"></span>
                         </NavLink>
                     </div>
                 </div>
